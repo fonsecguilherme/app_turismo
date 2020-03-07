a1b8269bd637f22f0fe1bd7f0288fb89958844f3
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2020-03-06 22:27:21 -0300

    adicionando barra semi transparente

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,7 +1,10 @@
 import 'package:app_turismo/style.dart';
 import 'package:flutter/material.dart';
+import 'package:app_turismo/models/location.dart';
 
 class WelcomeScreen extends StatelessWidget {
+  final location = Location();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,25 +25,57 @@ class WelcomeScreen extends StatelessWidget {
               color: Colors.transparent,
               width: 302.0,
               height: 340.0,
-              child: new Container(
-                decoration: new BoxDecoration(
-                  image: new DecorationImage(
-                    image: new AssetImage("assets/images/maceio.jpg"),
-                    fit: BoxFit.cover,
+              child: Stack(
+                children: <Widget>[
+                  new Container(
+                    decoration: new BoxDecoration(
+                      image: new DecorationImage(
+                        image: new AssetImage("assets/images/maceio.jpg"),
+                        fit: BoxFit.cover,
+                      ),
+                      color: Colors.red,
+                      borderRadius: BorderRadius.only(
+                        topLeft: const Radius.circular(20.0),
+                        topRight: const Radius.circular(20.0),
+                        bottomLeft: const Radius.circular(20.0),
+                        bottomRight: const Radius.circular(20.0),
+                      ),
+                    ),
                   ),
-                  color: Colors.red,
-                  borderRadius: BorderRadius.only(
-                    topLeft: const Radius.circular(20.0),
-                    topRight: const Radius.circular(20.0),
-                    bottomLeft: const Radius.circular(20.0),
-                    bottomRight: const Radius.circular(20.0),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 50.0),
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.5),
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: const Radius.circular(20.0),
+                            bottomRight: const Radius.circular(20.0),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
+                ],
               ),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget overlayBar() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(color: Colors.black.withOpacity(0.5)),
+        )
+      ],
     );
   }
 }
